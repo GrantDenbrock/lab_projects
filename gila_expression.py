ad4cc393e4889fe3efdc6d553313305fafa91c76
GrantDenbrock <grantdenbrock@icloud.com>
2018-08-10 19:11:30 -0700

    using classes instead of dicts

--- gila_expression.py
+++ gila_expression.py
@@ -1,44 +1,50 @@
-#for sample in sample list
-#and for each transcript in the sample_list
-#determine if sample is male or female using tags
-#then seperate and place values accordingly
-#if the transcript exists in transcript_dict
-#just append its values for FPKM and stuff
-#else append the new transcript number and also do its values of FPKM and stuff.
+input_files = []
 
-#might need to cut columns into lists like the other project. FIXME
-import os
+transcript_list = [] #functions as checklist for already counted tid's
 
-sample_list = []
-males = []
-females = []
-transcript_dict = {}
-data_dict = {}
+def parse_args(): #FIXME
+    parser = argparse.ArgumentParser(description="Input Samples")
+    parser.add_argument('gender: is male', type = str, help='yes if male, no if female')
+    parser.add_argument('--input_files', nargs='+', help = 'enter inputs separated by spaces followed by: --input_files')
+    args = parser.parse_args()
+    return args
 
-#so the structure is... data_dict has Key: transcript number and Value: another dictionary with Key: parameter and Value: Value
-#so when I go data_dict[transcript number[parameter]] --> I get out the value that I want. Do I want to be able to get the value per lizard? FIXME
-#lets say no for mow and just get something working.
+#make room for a class called transcripts
+class Transcripts:
+    """each transcript will get its own instantiation, and will hold all of the associated values."""
+    def __init__(self): #do I want to include gender, and other values here or just self?
 
-#CREATE SAMPLE LIST
-files = [f for f in os.listdir('.') if os.path.isfile(f)]
-for f in files:
-    if f.endswith('.stats')==True:
-        sample_list.append(f)
-print('printing sample list:')
-print(sample_list)
-
-#do you want to sort by male or female?
-#implement a --sort flag that will iterate samples in sample list and allow you to say whether is male or female FIXME
-
-#fill in transcripts lists
-transcrpits = []
-files = [f for f in os.listdir(os.curdir) if os.path.isfile(f)] # creates a list of files in current directory
-#print(files)
-for f in files: #for each file in current directory
-    if f.endswith('.transcripts.txt')==True: #and if it ends with .data.txt
-        with open(f , 'r') as readfile: #open the file f as readfile
-            for line in readfile: #for value in the readfile
-                transcript=line.strip() #variable number can take the value of the line without all the bullshit newline characters #also need to get rid of the quotations, turning string into float.
-                #print(number)
-                transcripts.append(transcript)
-#and do the same for the other columns that we want in our data table
+for file in input_files:
+    with open(file,'r') as readfile:
+        for line in readfile:
+            if line == 1:
+                pass #because the first line is just the headers...
+            else:
+                if line.strip(' ')[1] not in transcript_list: # "if the content of the first line is not present in transcript list"
+                    transcript_list.append(line.strip(' ')[1]) #add it to the checklist if it already isnt there
+                    line.strip(' ')[1] = Transcripts() #instantiate the class. Will need to throw in some error handling too. FIXME
+                    line.strip(' ')[1].tid = line.strip('  ')[1]
+                    line.strip(' ')[1].chr = line.strip('  ')[2]
+                    line.strip(' ')[1].strand = line.strip('  ')[3]
+                    line.strip(' ')[1].start = line.strip('  ')[4]
+                    line.strip(' ')[1].end = line.strip('  ')[5]
+                    line.strip(' ')[1].t_name = line.strip('  ')[6]
+                    line.strip(' ')[1].num_exons = line.strip('  ')[7]
+                    line.strip(' ')[1].length = line.strip('  ')[8]
+                    line.strip(' ')[1].gene_id = line.strip('  ')[9]
+                    line.strip(' ')[1].gene_name = line.strip('  ')[10]
+                    line.strip(' ')[1].cov = line.strip('  ')[11]
+                    line.strip(' ')[1].fpkm =  line.strip('  ')[12]
+                else:
+                    #the class is already instantiated so just add to it...
+                    line.strip(' ')[1].chr.append(line.strip('  ')[2])
+                    line.strip(' ')[1].strand.append(line.strip('  ')[3])
+                    line.strip(' ')[1].start.append(line.strip('  ')[4])
+                    line.strip(' ')[1].end.append(line.strip('  ')[5])
+                    line.strip(' ')[1].t_name.append(line.strip('  ')[6]
+                    line.strip(' ')[1].num_exons.append(line.strip('  ')[7])
+                    line.strip(' ')[1].length.append(line.strip('  ')[8])
+                    line.strip(' ')[1].gene_id.append(line.strip('  ')[9])
+                    line.strip(' ')[1].gene_name.append(line.strip('  ')[10])
+                    line.strip(' ')[1].cov.append(line.strip('  ')[11])
+                    line.strip(' ')[1].fpkm.append(line.strip('  ')[12])
