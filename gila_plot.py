4ef58bbe161294740a38399673ef4f5a2174e637
GrantDenbrock <grantdenbrock@icloud.com>
2018-09-16 14:13:51 -0700

    init commit of gila_plot; makes two plots of f_m_ratio

--- gila_plot.py
+++ gila_plot.py
@@ -1,52 +1,47 @@
 import pandas as pd
 import numpy as np
+import matplotlib
+matplotlib.use('TkAgg')
 import matplotlib.mlab as mlab
 import matplotlib.pyplot as plt
 
+
 inputfile = "~/lab_projects/test.txt" #FIXME implement args for this
 
-df = pd.read_csv(inputfile, sep = '\t')
-df.to_html('plot_gila_df.html')
-df_list=df['f_m_ratio'].tolist()
-#print(df_list) #has a bunch of "inf"'s and "nan"'s !!
-new_df_list = [x for x in df_list if float(x) != 'nan'] #FIXME I cant get rid of the nans and infs!
-new_df_list = [x for x in df_list if float(x) != 'inf']
-new_df_list = [x for x in df_list if str(x) != 'nan']
-new_df_list = [x for x in df_list if str(x) != 'inf']
-new_df_list = [x for x in df_list if ~np.isnan(x)]
-new_df_list = [x for x in df_list if ~np.isinf(x)]
-new_df_list = [x for x in df_list if type(x) == float] # => they are all floats.
-#print(new_df_list)
-#I know that the nans and infs are floats.
-#I know they are there in my list because I print the list and see it with my eyes.
-try:
-    df_list.remove('nan') #but this says nan not in list!
-except:
-    pass
+df = pd.read_csv(inputfile, sep = '\t') #reads our input file into a df
+df_no_inf = df[~df.isin([np.nan, np.inf, -np.inf]).any(1)] #gets rid of nans and infs
+df_list=df_no_inf['f_m_ratio'].tolist() #throws it into a list for plotting
 
-try:
-    df_list.remove(nan) #and this says nan not defined. Clearly I dont understand something here.
-except:
-    pass
-try:
-    df_list.remove(inf)
-except:
-    pass
-df_list = list(filter(lambda x: x!= 'nan', df_list)) #nope
-print(df_list)
+df_nan = df.loc[pd.isnull(df.f_m_ratio)] #makes a dataframe of all the nan values. For later reference
+df_nan.to_html('nan_gila.html')
 
-#I would make a plot, but I havent got a working list out of the df yet.
-num_bins = 10
+df_inf = df[df.isin([np.nan, np.inf, -np.inf]).any(1)] #makes a dataframe of the inf values For later reference
+df_inf.to_html('inf_gila.html')
+
+num_bins = 1000 #FIXME implement args
 
 fig, ax = plt.subplots()
 
 # the histogram of the data
-n, bins, patches = ax.hist(new_df_list, num_bins = 10, color='orange')
+n, bins, patches = ax.hist(df_list, num_bins, color='orange', range = (0,3)) #FIXME implement args
 
-ax.set_xlabel('FPKM')
-ax.set_ylabel('Frequency')
-ax.set_title(r'FPKM Frequency of different')
+ax.set_xlabel('Female/Male FPKM Ratio')
+ax.set_ylabel('Count')
+ax.set_title(r'Count of Female to Male Ratio')
 
 # Tweak spacing to prevent clipping of ylabel
 fig.tight_layout()
 plt.show()
+fig.savefig('histogram_f_m_fpkm_count_0_3.png')
+
+#now make a second histogram of a different window
+n, bins, patches = ax.hist(df_list, num_bins, color='orange', range = (0,5)) #FIXME implement args
+
+ax.set_xlabel('Female/Male FPKM Ratio')
+ax.set_ylabel('Count')
+ax.set_title(r'Count of Female to Male Ratio')
+
+# Tweak spacing to prevent clipping of ylabel
+fig.tight_layout()
+plt.show()
+fig.savefig('histogram_f_m_fpkm_count_0_5.png')
